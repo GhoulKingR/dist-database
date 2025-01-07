e03ea4359f3710bda51da8b3b3ba92414a77544a
Chigozie Oduah <87097037+GhoulKingR@users.noreply.github.com>
2025-01-07 12:44:40 +0100

    added create agent route

--- agents.py
+++ agents.py
@@ -1,19 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 import sqlite3
 
 app = Flask(__name__)
 
-# C
+# C [x]
 # R [x]
 # U
 # D
 
+# Get all agents
 @app.get("/api/agents")
 def allagents():
-    result = []
     try:
         conn = sqlite3.connect("databases/grp9agents.db")
         cursor = conn.execute("SELECT * FROM GRP9AGENTS")
+        result = []
         for row in cursor:
             result.append({
                 "code": row[0],
@@ -30,26 +31,27 @@ def allagents():
         err_return = {
             "error": err
         }
-        return err_return, 500
+        return err_return, 500  # internal server error
     
     finally:
         if conn:
             conn.close()
             print("Sqlite3 connection closed")
 
+# Get a single agent
 @app.get("/api/agents/<code>")
 def agent(code):
-    result = {}
     try:
         conn = sqlite3.connect("databases/grp9agents.db")
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM GRP9AGENTS WHERE agent_code = ?", (code,))
         row = cursor.fetchone()
 
+        result = {}
         if row == None:
             print(f'Agent with code "{code}" doesn\'t exist')
 
-            return result, 404
+            return result, 404  # not found
         else:
             result["code"] = row[0]
             result["name"] = row[1]
@@ -62,15 +64,68 @@ def agent(code):
     
     except sqlite3.Error as err:
         print("An SQL error occured:", err)
-        return {}, 500
+        return {}, 500  # Internal server error
     
     finally:
         if conn:
             conn.close()
             print("Sqlite3 connection closed")
 
+# Converting a number to agent code
+# For example: '3' -> 'A003' or '3124' -> 'A3124'
+def id_to_code(id):
+    id = str(id)
+    count = len(id)
+    if count < 3:
+        return 'A' + ('0' * (3 - count)) + id
+    
+    return 'A' + id
+
+def count_agents(conn):
+    cursor = conn.execute("SELECT * FROM GRP9AGENTS")
+    count = 0
+    for _ in cursor:
+        count += 1
+    return count
+
+# Route to create a new document
+@app.post("/api/agents")
+def create_agent():
+    try:
+        conn = sqlite3.connect("databases/grp9agents.db")
+        count = count_agents(conn)
+        
+        json_data = request.get_json()
+        if json_data:
+            code = id_to_code(count + 1)
+            conn.execute(
+                "INSERT INTO GRP9AGENTS VALUES (?, ?, ?, ?, ?, ?)",
+                (code, json_data["name"], json_data["working_area"], json_data["commission"], json_data["phone_no"], json_data["country"])
+            )
+            conn.commit()
+
+            json_data["code"] = code
+            return json_data
+        else:
+            return json_data, 400   # Bad request
+    
+    except sqlite3.Error as err:
+        print("An SQL error occured:", err)
+        return {}, 500  # internal server error
+
+    except AttributeError as err:
+        print("Attribute error: err")
+        return {}, 400  # Bad request
+    
+    finally:
+        if conn:
+            conn.close()
+            print("Sqlite3 connection closed")
+
+# Route to delete a document
+# Route to update a document
+
 def create_app():
-    global app
     return app
 
 if __name__ == "__main__":
--- tests/test_agents.py
+++ tests/test_agents.py
@@ -1,7 +1,8 @@
-def test_get_all(client):
+from agents import id_to_code, count_agents
+
+def test_get_all(client, agentdb):
     response = client.get("/api/agents")
-    print(response)
-    assert len(response.json) == 12
+    assert len(response.json) == count_agents(agentdb)
 
 def test_wrong_call_fails(client):
     response = client.get("/api/agents/233")
@@ -10,3 +11,19 @@ def test_wrong_call_fails(client):
 def test_right_is_successful(client):
     response = client.get("/api/agents/A002")
     assert response.json["code"] == "A002"
+
+def test_can_create_agent(client):
+    response = client.post("/api/agents", json={
+        "name": "Chigozie Oduah",
+        "working_area": "Tenesse",
+        "commission": "0.50",
+        "phone_no": "077-02346674",
+        "country": "",
+    })
+    assert response.status_code == 200
+
+def test_new_id():
+    assert id_to_code(3) == "A003"
+
+def test_works_on_gt_4():
+    assert id_to_code(2000) == "A2000"
